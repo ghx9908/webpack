e74b9cc57caa70199c2cd154a35ea1e4b24ef1cb
“hongxiang.gao” <hongxiang.gao@retech.work>
2023-03-01 10:20:11 +0800

    feat: 处理不是语句块的情况

--- 3.AST/02.js
+++ 3.AST/02.js
@@ -9,15 +9,19 @@ let arrowFunctionPlugin1 = {
     //如果是箭头函数，那么就会进来此函数，参数是箭头函数的节点路径对象
     ArrowFunctionExpression(path) {
       let { node } = path
+      let body = node.body
+      //如果函数体不是语句块
+      if (!types.isBlockStatement(body)) {
+        node.body = types.blockStatement([types.returnStatement(body)])
+      }
       node.type = "FunctionExpression"
     },
   },
 }
 
 let sourceCode = `
-const sum = (a, b) => {
-   return a + b;
-}
+const sum = (a, b) => a + b;
+
 `
 let targetSource = core.transform(sourceCode, {
   plugins: [arrowFunctionPlugin1],
