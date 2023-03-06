d75cc14b00be17eab60b0af1d3af0787c3280735
“hongxiang.gao” <hongxiang.gao@retech.work>
2023-03-06 17:24:08 +0800

    feat: webpack 打包

---
+++ 1.usage/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+module.exports = {
+  mode: "development",
+  devtool: false,
+  entry: "./src/index.js",
+  // entry: ["./src/entry1.js", "./src/entry2.js"],
+
+  // entry: {
+  //   entry1: "./src/entry1.js",
+  //   entry2: "./src/entry2.js",
+  // },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    // filename: "[name].js",
+    filename: "bundle.js",
+    clean: true, //在新的打包之前清除历史文件
+  },
+  devServer: {
+    host: "localhost", //主机名
+    port: 9000, //访问端口号
+    open: true, //构建结束后自动打开浏览器预览项目
+    compress: true, //启动gzip压缩
+    hot: true, //启动支持模块热替换
+    watchFiles: [
+      //监听这些文件的变化，如果这些文件变化了，可以重新编译
+      "src/**/*.js",
+    ], //如果不配置watchFiles就是监听所有的文件
+    //不管访问哪个路径，都会把请求重定向到index.html，交给前端路由来进行处理
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    // new HtmlWebpackPlugin({
+    //   template: "./src/entry1.html",
+    //   filename: "entry1.html",
+    //   chunks: ["entry1"],
+    // }),
+    // new HtmlWebpackPlugin({
+    //   template: "./src/entry2.html",
+    //   filename: "entry2.html",
+    //   chunks: ["entry2"],
+    // }),
+  ],
+}
