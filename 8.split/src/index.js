60ffbbc8939005209bff1fbe8f654d3efab18f10
“hongxiang.gao” <hongxiang.gao@retech.work>
2023-03-13 00:06:48 +0800

    feat:动态加载

---
+++ 8.split/src/index.js
@@ -0,0 +1,5 @@
+document.querySelector("#play").addEventListener("click", () => {
+  import("./video").then((result) => {
+    console.log(result.default)
+  })
+})
---
+++ 8.split/webpack.config.js
@@ -0,0 +1,21 @@
+/**
+ * @type {import('webpack').Configuration}
+ */
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+module.exports = {
+  mode: "development",
+  devtool: false,
+  output: {
+    clean: true,
+  },
+  /*  entry: {
+    page1: "./src/page1.js",
+    page2: "./src/page2.js",
+  }, */
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+}
