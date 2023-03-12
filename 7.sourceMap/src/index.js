a5b9858850c1ed42e72827beac1c2954a9b6493a
“hongxiang.gao” <hongxiang.gao@retech.work>
2023-03-12 23:45:06 +0800

    feat: sourcemap

---
+++ 7.sourceMap/src/index.js
@@ -0,0 +1,4 @@
+import { sum } from "./sum"
+debugger
+sum(1, 2)
+//console.log(window.a.b);
---
+++ 7.sourceMap/webpack.config.js
@@ -0,0 +1,33 @@
+/**
+ * @type {import('webpack').Configuration}
+ */
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  output: {
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
+}
