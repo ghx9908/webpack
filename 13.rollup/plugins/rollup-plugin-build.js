911ddc70283d6c14bbed3c38e2d7b7cd8411daf2
gaohongxiang <2460013249@qq.com>
2024-01-21 10:02:57 +0800

    feat: rollup

--- 13.rollup/plugins/rollup-plugin-build.js
+++ 13.rollup/plugins/rollup-plugin-build.js
@@ -11,16 +11,16 @@ function build() {
     },
     // 第一个
     async options(inputOptions) {
-      console.log('options',inputOptions);
+      console.log('options');
       //inputOptions.input = './src/main.js';
     },
     // 开始
     async buildStart(inputOptions) {
-      console.log('buildStart',inputOptions);
+      console.log('buildStart')
     },
     // 解析路径
     async resolveId(source, importer) {
-      console.log('resolveId', source, importer);
+      console.log('resolveId');
       if (source === 'virtual') {
         console.log('resolveId', source);
         //如果resolveId钩子有返回值了，那么就会跳过后面的查找逻辑，以此返回值作为最终的模块ID
@@ -29,7 +29,7 @@ function build() {
     },
     //加载此模块ID对应的内容
     async load(id) {
-      console.log('load=>',id)
+      console.log('load',)
       if (id === 'virtual') {
         console.log('load', id);
         return `export default "virtual"`;
@@ -37,21 +37,21 @@ function build() {
     },
     // 是否转换缓存的模块
     async shouldTransformCachedModule({ id, code, ast }) {
-      console.log('shouldTransformCachedModule', id, code, ast);
+      console.log('shouldTransformCachedModule');
       //不使用缓存，再次进行转换
       return true;
     },
     // 转换模块内容
     async transform(code, id) {
-      console.log('transform',code,id);
+      console.log('transform');
     },
     // 解析模块ID
     async moduleParsed(moduleInfo) {
-      console.log('moduleInfo',moduleInfo);
+      console.log('moduleInfo');
     },
     // 解析动态导入
     async resolveDynamicImport(specifier, importer) {
-      console.log('resolveDynamicImport', specifier, importer);
+      console.log('resolveDynamicImport');
     },
     // 结束
     async buildEnd() {
--- 13.rollup/rollup.config.js
+++ 13.rollup/rollup.config.js
@@ -1,11 +1,12 @@
 import build from './plugins/rollup-plugin-build.js';
-
+import generation from './plugins/rollup-plugin-generation.js';
 export default {
   input: "./src/index.js",
   output: {
     dir: 'dist',
   },
   plugins: [
-    build() // 调用自定义的构建插件
+    build(), // 调用自定义的构建插件
+    generation()
   ]
 }
